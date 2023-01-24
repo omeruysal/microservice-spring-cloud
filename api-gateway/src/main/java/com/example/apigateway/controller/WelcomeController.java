d2cdfa1d27e6e676220c12569a44e3b303d6b066
Omer <teouysal@outlook.com>
2023-01-24 18:44:30 +0300

    added api gateway

---
+++ api-gateway/src/main/java/com/example/apigateway/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to api-gateway";
+    }
+}
